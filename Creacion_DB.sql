97fb67824cc914d969b4a10184c2eaeef8bd3189
melinavicente <melinavicente@hotmail.com.ar>
2026-05-27 16:53:07 -0300

    Creación de las tablas Institución, Director, Departamento, Carrera, Alumnos. (Alumnos bien hecha, lo anterior era solo una prueba) 7 tablas restantes por crear.

--- Creacion_DB.sql
+++ Creacion_DB.sql
@@ -4,14 +4,50 @@ go
 Use Universidad_BD 
 go
 
-Create table Alumnos (
-	DNI int Identity (1,1) primary key,
-	Nombre varchar(50) NOT NULL,
-	Apellido varchar(50) NOT NULL,
-	Telefono varchar(100), 
-	Email varchar (100) NOT NULL,
-	Domicilio varchar (200),
-	Fecha_Nacimiento DATE
-	--FOREIGN KEY (ID_Carrera) REFERENCES Carrera(ID_Carrera)
+Create table Institucion (
+    ID_Institucion int identity (1,1) primary key,
+    Nombre varchar(50) not null,
+    Telefono varchar(20),
+    Email varchar(50)
 )
 
+Create table Director (
+    ID_Director int identity (1,1) primary key,
+    Nombre varchar(50) not null,
+    Apellido varchar(50) not null,
+    DNI varchar(15) not null,
+    Sueldo decimal(18,2),
+    Materia varchar(50)
+)
+
+Create table Departamento (
+    ID_Depto int identity (1,1) primary key,
+    Nombre varchar(100) not null,
+    Telefono varchar(20),
+    Email varchar(50),
+    ID_Director int,
+    ID_Institucion int,
+    FOREIGN KEY (ID_Director) REFERENCES Director(ID_Director),
+    FOREIGN KEY (ID_Institucion) REFERENCES Institucion(ID_Institucion) 
+)
+
+Create table Carrera (
+    ID_Carrera int identity (1,1) primary key,
+    Nombre varchar(100) not null,
+    Cantidad_Materias int not null,
+    ID_Depto int,
+    FOREIGN KEY (ID_Depto) REFERENCES Departamento(ID_Depto)
+)
+
+Create table Alumnos (
+    Legajo_Alumno int identity (1,1) primary key,
+    DNI varchar(15) not null,
+    Nombre varchar(50) NOT NULL,
+    Apellido varchar(50) NOT NULL,
+    Telefono varchar(20), 
+    Email varchar(50) NOT NULL,
+    Domicilio varchar(100),
+    Fecha_Nacimiento DATE,
+    ID_Carrera int,
+    FOREIGN KEY (ID_Carrera) REFERENCES Carrera(ID_Carrera)
+)
