215513a0a74344195fdb6012950531c62a50674f
rociosantini18 <rociosantini18@gmail.com>
2026-05-29 20:52:07 -0300

    Creacion de tablas Materia, Docentes, Catedra, Calificaciones e Innscripcion

--- Creacion_DB.sql
+++ Creacion_DB.sql
@@ -15,7 +15,7 @@ Create table Director (
     ID_Director int identity (1,1) primary key,
     Nombre varchar(50) not null,
     Apellido varchar(50) not null,
-    DNI varchar(15) not null,
+    DNI varchar(15) unique not null,
     Sueldo decimal(18,2),
     Materia varchar(50)
 )
@@ -24,7 +24,7 @@ Create table Departamento (
     ID_Depto int identity (1,1) primary key,
     Nombre varchar(100) not null,
     Telefono varchar(20),
-    Email varchar(50),
+    Email varchar(50) unique,
     ID_Director int,
     ID_Institucion int,
     FOREIGN KEY (ID_Director) REFERENCES Director(ID_Director),
@@ -41,13 +41,68 @@ Create table Carrera (
 
 Create table Alumnos (
     Legajo_Alumno int identity (1,1) primary key,
-    DNI varchar(15) not null,
+    DNI varchar(15) unique not null,
     Nombre varchar(50) NOT NULL,
     Apellido varchar(50) NOT NULL,
     Telefono varchar(20), 
-    Email varchar(50) NOT NULL,
+    Email varchar(50) unique NOT NULL,
     Domicilio varchar(100),
     Fecha_Nacimiento DATE,
     ID_Carrera int,
     FOREIGN KEY (ID_Carrera) REFERENCES Carrera(ID_Carrera)
 )
+
+Create table Materia (
+    ID_Materia int identity (1,1) primary key,
+    Nombre_Materia varchar(100) NOT NULL,
+    Horas_Semanales int NOT NULL,
+    ID_Carrera int,
+    FOREIGN KEY (ID_Carrera) REFERENCES Carrera(ID_Carrera)
+)
+
+Create table Docentes (
+    ID_Docente int identity (1,1) primary key,
+    Nombre varchar(50) NOT NULL,
+    Apellido varchar(50) NOT NULL,
+    Telefono varchar(20), 
+    Email varchar(50) unique NOT NULL,
+    Sueldo decimal(18,2) NOT NULL,
+    ID_Carrera int,
+    FOREIGN KEY (ID_Carrera) REFERENCES Carrera(ID_Carrera)
+)
+
+Create table Catedra (
+    ID_Catedra int identity (1,1) primary key,
+    ID_Materia int,
+    ID_Docente int,
+    Turno varchar(20) NOT NULL,
+    Año_Lectivo int NOT NULL,
+    FOREIGN KEY (ID_Materia) REFERENCES Materia(ID_Materia),
+    FOREIGN KEY (ID_Docente) REFERENCES Docentes(ID_Docente)
+)
+
+Create table Calificaciones (
+    ID_Nota int identity (1,1) primary key,
+    ID_Materia int,
+    Legajo_Alumno int,
+    Calificacion decimal(4,2),
+    Fecha_Calificacion DATE,
+    FOREIGN KEY (ID_Materia) REFERENCES Materia(ID_Materia),
+    FOREIGN KEY (Legajo_Alumno) REFERENCES Alumnos(Legajo_Alumno)
+)
+
+
+
+Create table Inscripcion (
+    ID_Inscripcion int identity (1,1) primary key,
+    ID_Catedra int,
+    Legajo_Alumno int,
+    Estado bit,
+    Fecha_Inscripcion DATE,
+    Ciclo_Lectivo int,
+    FOREIGN KEY (ID_Catedra) REFERENCES Catedra(ID_Catedra),
+    FOREIGN KEY (Legajo_Alumno) REFERENCES Alumnos(Legajo_Alumno)
+)
+
+
+
