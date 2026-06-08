991e312787608b5fe133492163e6bb8d24e81ed3
melinavicente <melinavicente@hotmail.com.ar>
2026-06-08 16:42:50 -0300

    Creación de otros dos procedimientos almacenados: Modificar alumno y buscarlo por apellido.

--- STORE_PROCEDURE.sql
+++ STORE_PROCEDURE.sql
@@ -2,7 +2,7 @@ use Universidad_BD
 go
 
 --Dar de alta un alumno en el sistema
-create procedure sp_AltaAlumno 
+/*create procedure sp_AltaAlumno 
 @dni varchar(15),
 @nombre varchar (50),
 @apellido varchar (50),
@@ -58,10 +58,96 @@ begin try
 
 select * from vw_AlumnosCarrera
 
-
+go */
 --sp_ModificarAlumno
---sp_BajaAlumno
+--Modificar los datos basicos de un alumno: Telefono, email,domicilio o la carrera.
+	create procedure sp_ModificarAlumno
+	@legajoAlumno int,
+	@telefono varchar(20),
+	@email varchar (50),
+	@domicilio varchar(100),
+	@ID_Carrera int 
+	as begin 
+	if not exists (select 1 from Alumnos where Legajo_Alumno = @legajoAlumno)
+	begin
+	raiserror ('No existe un alumno con ese numero de legajo', 16,1)
+	return
+	end
+
+	if exists (select 1 from Personas p 
+	inner join Alumnos a on p.ID_Persona = a.ID_Persona
+	where p.Email = @email and a.Legajo_Alumno = @legajoAlumno )
+	begin
+	raiserror ('El email ingresado ya pertenece a otro alumno', 16, 1)
+	return
+	end
+
+	if not exists (select 1 from Carrera where ID_Carrera = @ID_Carrera)
+	begin
+	raiserror ('La carrera seleccionada no existe', 16, 1)
+	return
+	end
+
+	begin transaction 
+	begin try 
+		update Personas
+		set Telefono = @telefono,
+		Email = @email,
+		Domicilio = @domicilio
+		where ID_Persona = 
+		(select ID_Persona from Alumnos where Legajo_Alumno = @legajoAlumno)
+
+		update Alumnos 
+		set ID_Carrera = @ID_Carrera
+		where Legajo_Alumno = @legajoAlumno
+
+	commit transaction 
+	print 'Alumno modificado correctamente!'
+	end try
+
+	begin catch
+	rollback transaction 
+	raiserror ('Ocurrio un error al modificar al alumnos',16,1)
+	end catch 
+	end 
+	go
+
+	exec sp_ModificarAlumno 1,'114585788','gonzdle2@gamil.com','Beltran al 245',3
+	select * from vw_AlumnosCarrera
+
 --sp_BuscarAlumnoApellido
+--Se busca un alumno por su apellido 
+	alter procedure sp_BuscarAlumnoApellido 
+	@Apellido varchar(50)
+	as begin 
+
+	if @Apellido is null or trim(@Apellido) = ''
+	begin 
+	raiserror ('Debe ingresar un apellido para realizar la busqueda',16,1)
+	return
+	end
+
+	select 
+        a.Legajo_Alumno,
+        p.DNI,
+        p.Apellido,
+        p.Nombre,
+        p.Email,
+        p.Telefono,
+        c.Nombre as Carrera
+    from Alumnos a
+    inner join Personas p on a.ID_Persona = p.ID_Persona
+    left join Carrera c on a.ID_Carrera = c.ID_Carrera
+    where p.Apellido like '%' + @Apellido + '%'
+    order by p.Apellido, p.Nombre;
+end
+go
+
+exec sp_BuscarAlumnoApellido 'Vicente';
+exec sp_BuscarAlumnoApellido '';
+go
+
+--sp_BajaAlumno
 --sp_InscribirAlumnoMateria
 --sp_RegistrarAsistencia
 --sp_CargarNota
